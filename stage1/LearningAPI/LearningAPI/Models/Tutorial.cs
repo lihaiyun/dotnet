866984494ab0f4eda161457d547a8b50c4eb4a8b
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-03-22 18:18:32 +0800

    Add annotations

--- stage1/LearningAPI/LearningAPI/Models/Tutorial.cs
+++ stage1/LearningAPI/LearningAPI/Models/Tutorial.cs
@@ -1,15 +1,19 @@
-﻿namespace LearningAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningAPI.Models
 {
     public class Tutorial
     {
         public int Id { get; set; }
 
+        [Required, MaxLength(100)]
         public string Title { get; set; } = string.Empty;
 
+        [Required, MaxLength(500)]
         public string Description { get; set; } = string.Empty;
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
