b6c855b960680e6f3f160ebc286d1ce17641bf2b
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-03-23 16:51:08 +0800

    User register

---
+++ stage2/LearningAPI/LearningAPI/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using LearningAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public UserController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/User/register
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var foundUser = _context.Users.Where(x => x.Email == request.Email).FirstOrDefault();
+            if (foundUser != null)
+            {
+                return BadRequest("Email already exists.");
+            }
+
+            var now = DateTime.Now;
+            var user = new User() {
+                Name = request.Name,
+                Email = request.Email,
+                Password = request.Password,
+                CreatedAt = now,
+                UpdatedAt = now
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+    }
+}
