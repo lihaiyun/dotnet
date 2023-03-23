8eebcb79d54185e199c8f219ab89f0b510f6875e
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-03-23 13:20:59 +0800

    copy from stage1

---
+++ stage2/LearningAPI/LearningAPI/Models/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningAPI.Models
+{
+    public class Tutorial
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required, MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
