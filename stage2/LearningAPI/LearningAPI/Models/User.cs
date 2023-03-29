90b1f3b5d132b73c65761d01c2b289cf57aaa0b6
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-03-29 17:59:14 +0800

    update user

--- stage2/LearningAPI/LearningAPI/Models/User.cs
+++ stage2/LearningAPI/LearningAPI/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace LearningAPI.Models
 {
@@ -6,10 +7,13 @@ namespace LearningAPI.Models
     {
         public int Id { get; set; }
 
+        [Required, MinLength(3), MaxLength(50)]
         public string Name { get; set; } = string.Empty;
 
+        [Required, EmailAddress, MaxLength(50)]
         public string Email { get; set; } = string.Empty;
 
+        [Required, MinLength(8), MaxLength(100)]
         public string Password { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; }
