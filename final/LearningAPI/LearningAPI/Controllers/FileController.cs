6486c6e2c05fafcbfb00911c6af6921d983cf43e
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-03 15:49:32 +0800

    file upload

---
+++ final/LearningAPI/LearningAPI/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using LearningAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private IWebHostEnvironment _environment;
+
+        public FileController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        // POST: api/File/upload
+        [HttpPost("upload"), Authorize]
+        public IActionResult Upload(IFormFile file)
+        {
+            if (file.Length > 1024 * 1024)
+            {
+                return BadRequest("File size cannot exceed 1MB.");
+            }
+
+            var id = Nanoid.Nanoid.Generate(size:10);
+            var fileName = id + Path.GetExtension(file.FileName);
+            var imagePath = Path.Combine(_environment.ContentRootPath, "wwwroot//uploads", fileName);
+            using var fileStream = new FileStream(imagePath, FileMode.Create);
+            file.CopyTo(fileStream);
+
+            return Ok(new { fileName });
+        }
+    }
+}
