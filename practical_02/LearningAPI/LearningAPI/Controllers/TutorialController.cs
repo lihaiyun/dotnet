3b2070a31a1426bdfb9b6e0617f5272854f665ca
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-11 17:25:13 +0800

    copy from practical 1

---
+++ practical_02/LearningAPI/LearningAPI/Controllers/TutorialController.cs
@@ -0,0 +1,25 @@
+﻿using LearningAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TutorialController : ControllerBase
+    {
+        private static readonly List<Tutorial> list = new();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult AddTutorial(Tutorial tutorial)
+        {
+            list.Add(tutorial);
+            return Ok(tutorial);
+        }
+    }
+}
