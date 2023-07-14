21f8410b1cc7886e6657b7d9b65755df757c015c
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-14 14:03:49 +0800

    Set up Entity Framework Core

--- practical_02/LearningAPI/LearningAPI/Models/Tutorial.cs
+++ practical_02/LearningAPI/LearningAPI/Models/Tutorial.cs
@@ -2,8 +2,14 @@
 {
     public class Tutorial
     {
+        public int Id { get; set; }
+
         public string Title { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
