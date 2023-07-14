5d7a67b30be6a903d7fcaf0c9b735fbfdc56a6e8
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-14 16:46:16 +0800

    add react app

---
+++ practical_03/learning-react-app/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Tutorials from './pages/Tutorials';
+import AddTutorial from './pages/AddTutorial';
+import EditTutorial from './pages/EditTutorial';
+
+function App() {
+  return (
+    <Router>
+      <AppBar position="static" className="AppBar">
+        <Container>
+          <Toolbar disableGutters={true}>
+            <Link to="/">
+              <Typography variant="h6" component="div">
+                Learning
+              </Typography>
+            </Link>
+            <Link to="/tutorials" ><Typography>Tutorials</Typography></Link>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Container>
+        <Routes>
+          <Route path={"/"} element={<Tutorials />} />
+          <Route path={"/tutorials"} element={<Tutorials />} />
+          <Route path={"/addtutorial"} element={<AddTutorial />} />
+          <Route path={"/edittutorial/:id"} element={<EditTutorial />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
