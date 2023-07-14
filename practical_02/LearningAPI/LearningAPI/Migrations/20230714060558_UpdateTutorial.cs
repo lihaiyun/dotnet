bcdfa2e996163d093c2323f846e7def19eb931b3
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-14 14:11:34 +0800

    update tutorial

---
+++ practical_02/LearningAPI/LearningAPI/Migrations/20230714060558_UpdateTutorial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearningAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTutorial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Tutorials",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tutorials",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Tutorials",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tutorials",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
--- practical_02/LearningAPI/LearningAPI/Models/Tutorial.cs
+++ practical_02/LearningAPI/LearningAPI/Models/Tutorial.cs
@@ -1,11 +1,15 @@
-﻿namespace LearningAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningAPI.Models
 {
     public class Tutorial
     {
         public int Id { get; set; }
 
+        [Required, MinLength(3), MaxLength(100)]
         public string Title { get; set; } = string.Empty;
 
+        [Required, MinLength(3), MaxLength(500)]
         public string Description { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; }
