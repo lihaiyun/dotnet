2011497fb24944579db2a9bf55c69517317f9d8f
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-14 15:22:32 +0800

    copy from practical 2

---
+++ practical_03/LearningAPI/LearningAPI/Controllers/TutorialController.cs
@@ -0,0 +1,47 @@
+﻿using LearningAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TutorialController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public TutorialController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(string? search)
+        {
+            IQueryable<Tutorial> result = _context.Tutorials;
+            if (search != null)
+            {
+                result = result.Where(x => x.Title.Contains(search)
+                    || x.Description.Contains(search));
+            }
+            var list = result.OrderByDescending(x => x.CreatedAt).ToList();
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult AddTutorial(Tutorial tutorial)
+        {
+            var now = DateTime.Now;
+            var myTutorial = new Tutorial()
+            {
+                Title = tutorial.Title.Trim(),
+                Description = tutorial.Description.Trim(),
+                CreatedAt = now,
+                UpdatedAt = now
+            };
+
+            _context.Tutorials.Add(myTutorial);
+            _context.SaveChanges();
+            return Ok(myTutorial);
+        }
+    }
+}
