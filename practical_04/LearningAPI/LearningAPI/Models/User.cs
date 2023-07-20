32ef7b7ea57a1510b243214b4c8007bebc6959ec
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-20 09:52:38 +0800

    Add User

---
+++ practical_04/LearningAPI/LearningAPI/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearningAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string Email { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Password { get; set; } = string.Empty;
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
