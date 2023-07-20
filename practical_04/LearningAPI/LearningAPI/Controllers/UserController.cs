ecd2aa102a481e5fdaf7ee5030330d08dcb5eb2f
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-20 11:38:15 +0800

    Create UserController.cs

---
+++ practical_04/LearningAPI/LearningAPI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using LearningAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public UserController(MyDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
