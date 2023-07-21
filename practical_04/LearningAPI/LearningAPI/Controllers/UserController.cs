d550cbcc4b9ab66d7ba341e0b337c265a8768f27
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-21 15:31:40 +0800

    add post method for user register

--- practical_04/LearningAPI/LearningAPI/Controllers/UserController.cs
+++ practical_04/LearningAPI/LearningAPI/Controllers/UserController.cs
@@ -13,5 +13,39 @@ namespace LearningAPI.Controllers
         {
             _context = context;
         }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            // Trim string values
+            request.Name = request.Name.Trim();
+            request.Email = request.Email.Trim().ToLower();
+            request.Password = request.Password.Trim();
+
+            // Check email
+            var foundUser = _context.Users.Where(x => x.Email == request.Email).FirstOrDefault();
+            if (foundUser != null)
+            {
+                string message = "Email already exists.";
+                return BadRequest(new { message });
+            }
+
+            // Create user object
+            var now = DateTime.Now;
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            var user = new User()
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Password = passwordHash,
+                CreatedAt = now,
+                UpdatedAt = now
+            };
+
+            // Add user
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
---
+++ practical_04/LearningAPI/LearningAPI/Models/RegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningAPI.Models
+{
+    public class RegisterRequest
+    {
+        [Required, MinLength(3), MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, EmailAddress, MaxLength(50)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(8), MaxLength(50)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
