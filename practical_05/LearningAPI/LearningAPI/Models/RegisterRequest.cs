1958b77429ad2cbd7e8630cd15a4e5946353acd9
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-07-31 13:30:24 +0800

    Use regular expression

--- practical_05/LearningAPI/LearningAPI/Models/RegisterRequest.cs
+++ practical_05/LearningAPI/LearningAPI/Models/RegisterRequest.cs
@@ -5,12 +5,18 @@ namespace LearningAPI.Models
     public class RegisterRequest
     {
         [Required, MinLength(3), MaxLength(50)]
+        // Regular expression to enforce name format
+        // letters, spaces, apostrophes, hyphens, commas and periods
+        [RegularExpression("^[a-zA-Z '-,.]+$", ErrorMessage = "Invalid name")]
         public string Name { get; set; } = string.Empty;
 
         [Required, EmailAddress, MaxLength(50)]
         public string Email { get; set; } = string.Empty;
 
         [Required, MinLength(8), MaxLength(50)]
+        // Regular expression to enforce password complexity
+        // at least 1 letter and 1 number, and 8-50 characters
+        [RegularExpression("^(?=.*?[a-zA-Z])(?=.*?[0-9]).{8,50}$", ErrorMessage = "Invalid password")]
         public string Password { get; set; } = string.Empty;
     }
 }
