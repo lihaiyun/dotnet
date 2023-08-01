8f611f18d69e6b3fb6db578bed398e6851b8e950
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-08-01 16:34:17 +0800

    Create FileController.cs

---
+++ practical_06/LearningAPI/LearningAPI/Controllers/FileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private IWebHostEnvironment _environment;
+
+        public FileController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+    }
+}
