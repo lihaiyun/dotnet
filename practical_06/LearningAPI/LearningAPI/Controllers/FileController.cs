83a320fa24958dfeafb0fcd9bc5108e63ed33fa7
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-08-01 16:53:37 +0800

    Add method Upload

--- practical_06/LearningAPI/LearningAPI/Controllers/FileController.cs
+++ practical_06/LearningAPI/LearningAPI/Controllers/FileController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NanoidDotNet;
 
 namespace LearningAPI.Controllers
 {
@@ -11,6 +13,23 @@ namespace LearningAPI.Controllers
         public FileController(IWebHostEnvironment environment)
         {
             _environment = environment;
+        }
+
+        [HttpPost("upload"), Authorize]
+        public IActionResult Upload(IFormFile file)
+        {
+            if (file.Length > 1024 * 1024)
+            {
+                var message = "Maximum file size is 1MB";
+                return BadRequest(new { message });
+            }
+
+            var id = Nanoid.Generate(size: 10);
+            var filename = id + Path.GetExtension(file.FileName);
+            var imagePath = Path.Combine(_environment.ContentRootPath, @"wwwroot/uploads", filename);
+            using var fileStream = new FileStream(imagePath, FileMode.Create);
+            file.CopyTo(fileStream);
+            return Ok(new { filename });
         }
     }
 }
