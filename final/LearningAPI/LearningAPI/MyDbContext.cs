8cbd2e7486fdc45536d4e0d16ee0f28858905d9f
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-08-25 10:02:45 +0800

    Copy from practical 6

---
+++ final/LearningAPI/LearningAPI/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using LearningAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningAPI
+{
+    public class MyDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string? connectionString = _configuration.GetConnectionString("MyConnection");
+            if (connectionString != null)
+            {
+                optionsBuilder.UseMySQL(connectionString);
+            }
+        }
+
+        public DbSet<Tutorial> Tutorials { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
