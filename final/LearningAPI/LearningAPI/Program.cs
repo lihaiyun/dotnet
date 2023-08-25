bf5e972f3d049278b6e9569086522125cf56fde3
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-08-25 16:40:05 +0800

    Update Program.cs

--- final/LearningAPI/LearningAPI/Program.cs
+++ final/LearningAPI/LearningAPI/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-//builder.Services.AddLogging();
 builder.Services.AddDbContext<MyDbContext>();
 
 // Auto Mapper
